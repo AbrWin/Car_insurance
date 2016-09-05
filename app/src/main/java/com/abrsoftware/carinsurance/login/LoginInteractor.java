578b18103a2742695a79e8677be1d26af64e6de6
Abraham Sánchez Juárez <AbrWin@la-MacBook-Pro-de-Abraham.local>
2016-09-05 16:22:01 -0500

    se avanza con mvp LOGIN

--- app/src/main/java/com/abrsoftware/carinsurance/login/LoginInteractor.java
+++ app/src/main/java/com/abrsoftware/carinsurance/login/LoginInteractor.java
@@ -1,9 +1,117 @@
 package com.abrsoftware.carinsurance.login;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * Created by AbrWin on 11/08/16.
  */
 public class LoginInteractor {
+
+    private final Context mContext;
+    private FirebaseAuth mFirebaseAuth;
+
+    public LoginInteractor(Context context, FirebaseAuth firebaseAuth) {
+        mContext = context;
+        if (mFirebaseAuth != null) {
+            mFirebaseAuth = firebaseAuth;
+        } else {
+            throw new RuntimeException("La instancia de FirebaseAuth no puede ser null");
+        }
+    }
+
+    public void login(String mail, String password, final Callback callback) {
+        boolean c1 = isValidateEmail(mail, callback);
+        boolean c2 = isValidPassword(password, callback);
+
+        //Check validate input text
+        if (!(c1 && c2)) {
+            return;
+        }
+
+        //Check isNetworkAvailable
+        if(!isNetworkAvailable()){
+            callback.onNetworkConnectFailed();
+        }
+
+        //Check GooglePlayServices
+        if(!isGooglePlayServicesAvailable(callback)){
+            return;
+        }
+
+        // Consultar Firebase Authentication
+        signInUser(mail, password, callback);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) mContext.getSystemService(mContext.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private boolean isGooglePlayServicesAvailable(Callback callback){
+        int statuscode = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(mContext);
+        if(GoogleApiAvailability.getInstance().isUserResolvableError(statuscode)){
+            callback.onBeUserResolvableError(statuscode);
+            return false;
+        }else if(statuscode != ConnectionResult.SUCCESS){
+            callback.onGooglePlayServicesFailed();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidateEmail(String email, Callback callback) {
+        boolean isValid = true;
+        if (TextUtils.isEmpty(email)) {
+            callback.onEmailError("Escribe tu correo");
+            isValid = false;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            callback.onEmailError("Correo no válido");
+            isValid = false;
+        }
+        // Más reglas de negocio...
+        return isValid;
+    }
+
+    private boolean isValidPassword(String password, Callback callback) {
+        boolean isValid = true;
+        if (TextUtils.isEmpty(password)) {
+            callback.onPasswordError("Escribe tu contraseña");
+            isValid = false;
+        }
+
+        // Más reglas de negocio...
+        return isValid;
+    }
+
+    private void signInUser(String mail, String password, final Callback callback){
+        mFirebaseAuth.signInWithEmailAndPassword(mail, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(!task.isSuccessful()){
+                    callback.onAuthFailed(task.getException().getMessage());
+                }else{
+                    callback.onAuthSuccess();
+                }
+
+            }
+        });
+    }
+
     interface Callback {
 
         void onEmailError(String msg);
--- app/src/main/java/com/abrsoftware/carinsurance/login/LoginPresenter.java
+++ app/src/main/java/com/abrsoftware/carinsurance/login/LoginPresenter.java
@@ -1,9 +1,22 @@
 package com.abrsoftware.carinsurance.login;
 
+import android.support.annotation.NonNull;
+
 /**
  * Created by json on 17/08/16.
  */
 public class LoginPresenter implements LoginContract.Presenter, LoginInteractor.Callback{
+
+    private final LoginContract.View mLoginView;
+    private LoginInteractor mLoginInteractor;
+
+    public LoginPresenter(@NonNull LoginContract.View loginView,
+                          @NonNull LoginInteractor loginInteractor){
+        mLoginView = loginView;
+        loginView.setPresenter(this);
+        mLoginInteractor = loginInteractor;
+    }
+
     @Override
     public void attemptLogin(String mail, String password) {
 
